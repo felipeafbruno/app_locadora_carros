571a959600795d73731680ac933e9de4db6997a8
felipeafbruno <felipedobru@gmail>
2021-09-11 19:26:21 -0300

    iniciando a implementação do front com Vue, implentado web e JWT, login, componentes home e marcas e por fim foi adicionadas as funcionalidades de busca, listagem e adição de novas marcas

---
+++ resources/js/components/InputContainerComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="form-group">
+        <label :for="id" class="form-label">{{ titulo }}</label>
+        <!-- tag slot permite exibir o conteúdo interno passado no componente -->
+        <slot></slot>
+        <div :id="idHelp" class="form-text text-muted">{{ textoAjuda }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['id', 'titulo', 'idHelp', 'textoAjuda']
+    }
+</script>
